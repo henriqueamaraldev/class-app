ff6391e3ac1e9bac7289f9efa43fc5e84c6f11ef
Henrique Correa do Amaral <henriqueamaralbh13@gmail.com>
2022-03-15 09:53:33 -0300

    feat: modulo dos professores

---
+++ src/teachers/teachers.module.ts
@@ -0,0 +1,14 @@
+import { Module } from '@nestjs/common';
+import { TeachersService } from './teachers.service';
+import { TeachersController } from './teachers.controller';
+import { MongooseModule } from '@nestjs/mongoose';
+import { Teacher, TeachersSchema } from './entities/teachers.entity';
+
+@Module({
+  imports: [
+    MongooseModule.forFeature([{ name: Teacher.name, schema: TeachersSchema }]),
+  ],
+  providers: [TeachersService],
+  controllers: [TeachersController],
+})
+export class TeachersModule {}
