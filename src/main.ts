e0c488059723b287279a83b0a347fe790288687f
Henrique Correa do Amaral <henriqueamaralbh13@gmail.com>
2022-03-18 10:55:34 -0300

    feat: mensagem amigavel quando o servidor ligar

--- src/main.ts
+++ src/main.ts
@@ -4,5 +4,6 @@ import { AppModule } from './app.module';
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   await app.listen(process.env.PORT);
+  console.log('Welcome to Nest.JS class-app project:', process.env.PORT);
 }
 bootstrap();
